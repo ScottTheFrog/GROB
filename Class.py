bbe0a845b18f7a71597ff66d5d08136e67722e5f
ScottTheFrog <55336944+ScottTheFrog@users.noreply.github.com>
2020-04-06 19:02:35 -0400

    new damage and color

--- Class.py
+++ Class.py
@@ -9,28 +9,30 @@ class Grob():
 		self. health 	= health
 		self.speed	 	= speed
 		self.state 		= state
-		self.state 		= ["running","top"]
+		self.state 		= 2
+		self.running	= True
 	def move(self):
-		self.speed *= 1.0002
+		self.speed *= 1.0005
 		self.rect = pygame.Rect(self.x,self.y,12,10)
-		if self.state[0] == "running":
+
+		if self.running :	
 			for i in allrects:
 				i2 = allrects.get(i)
 				i2[0] -= self.speed
-				print (i2)
+
 		##keyboard
 		k = pygame.key.get_pressed()
-		if k[pygame.K_SPACE]:
-			self.state[1] = "top"
-		elif not k[pygame.K_SPACE]:
-			self.state[1] = "bot"
+		if k[pygame.K_UP]:
+			self.state = 1
+		if k[pygame.K_DOWN]:
+			self.state = 2
 
-		if self.state[1] == "top":
+		if self.state == 1:
 			if self.y != 32:
 				self.y -= self.speed*4
 			if self.y < 32:
 				self.y = 32
-		if self.state[1] == "bot":
+		if self.state == 2:
 			if self.y != 448:
 				self.y += self.speed*4
 			if self.y > 448:
@@ -39,7 +41,7 @@ class Grob():
 	def col(self):
 		for i in allrects:
 			if self.rect.colliderect(allrects.get(i)):
-				self.speed = 1
+				pygame.draw.rect(screen,[255,0,0],[0,0,640,480])
 class Block():
 	def __init__(self,x,y,w,h,rectname):
 		self.x 			= x
@@ -50,12 +52,12 @@ class Block():
 		self.rectname 	= rectname
 		self.rect = pygame.Rect(self.x,self.y,self.w,self.h)
 	def draw(self):
-
 		if self.rectname not in allrects:
 			allrects[self.rectname] = self.rect
 			self.rect = allrects.get(self.rectname)
 
-		pygame.draw.rect(screen,[255,0,0],self.rect)
+		pygame.draw.rect(screen,[120,0,0],self.rect)
+
 	def random(self):
 		if self.rect[0] < -40:
 			self.rect[0] = 1000
