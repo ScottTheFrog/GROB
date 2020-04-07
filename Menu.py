d710ea10d7bb26bbb2410eab7025da831112a1be
ScottTheFrog <55336944+ScottTheFrog@users.noreply.github.com>
2020-04-07 02:13:08 -0400

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -5,20 +5,15 @@ menuimg = pygame.transform.scale(menuimg, (640, 480))
 class loadmenu():
 	def __init__(self,song):
 		self.run = True
-		self.menu = True
-		self.color = pygame.Color(255,0,255,25)
+		self.menu = 0
+		self.color = pygame.Color(255,0,255,10)
 		self.song = song
 		self.pressed = 0
 		self.exit = False
-		self.play = pygame.Rect(292,290,348,20)
+		self.play = pygame.Rect(292,290,348,25)
 		self.options = pygame.Rect(292,324,348,20)
-		self.exit = pygame.Rect(600,400,30,30)
-
-		sound = pygame.mixer.Sound(self.song)
-		sound.play()
-		sound.set_volume(0.1)
+		self.exit = pygame.Rect(600,395,35,35)
 	def load(self):
-		print(self.pressed)
 		screen.blit(menuimg,[0,0])
 		m = pygame.mouse.get_pos()
 		mp = pygame.mouse.get_pressed()[0]
@@ -38,6 +33,10 @@ class loadmenu():
 
 		if mp:
 			if self.pressed == 1:
-				self.menu = False
+				self.menu = 1
 			if self.pressed == 3:
 				self.run = False
+	def playsnd(self):
+		sound = pygame.mixer.Sound(self.song)
+		sound.play()
+		sound.set_volume(0.1)
