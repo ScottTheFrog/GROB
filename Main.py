6972ed2a94e90d1ebd510c725400f9cd9dded913
ScottTheFrog <55336944+ScottTheFrog@users.noreply.github.com>
2020-04-07 00:32:28 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,19 @@
 import pygame  
 import Class
+import Menu
 import random
 
+
 pygame.init()
-run = True  
-clock = pygame.time.Clock()
 FPS = 45
+clock = pygame.time.Clock()
 playerimg = pygame.image.load("grob.png")
 screen = pygame.display.set_caption("-GROB-")
 screen = pygame.display.set_mode([640,480])
 
+menuglados = Menu.loadmenu("glados.wav")
 
-P1 = Class.Grob(64,380,1,11,"running")
+P1 = Class.Grob(64,380,1,5,"running")
 T  = Class.Block(400,10,40,-400,"a001")
 T1 = Class.Block(600,0,40,400,"a002")
 T2  = Class.Block(800,0,40,400,"a003")
@@ -19,31 +21,34 @@ T3 = Class.Block(1000,0,40,400,"a004")
 T4  = Class.Block(1200,0,40,400,"a005")
 
 	
-while run:
+while menuglados.run:
 	clock.tick(FPS)
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
-			run = False
+			menuglados.run = False
 
 	screen.fill((0,0,0))
 
-	pygame.draw.rect(screen,[255,255,255],[0,32,640,432])
-	P1.move()
-	P1.col()
+	if menuglados.menu:
+		menuglados.load()
+	else:
+		pygame.draw.rect(screen,[255,255,255],[0,32,640,432])
+		P1.move()
+		P1.col()
 
-	T.draw()
-	T1.draw()
-	T2.draw()
-	T3.draw()
-	T4.draw()
+		T.draw()
+		T1.draw()
+		T2.draw()
+		T3.draw()
+		T4.draw()
 
-	T.random()
-	T1.random()
-	T2.random()
-	T3.random()  
-	T4.random() 
+		T.random()
+		T1.random()
+		T2.random()
+		T3.random()  
+		T4.random() 
 
-	screen.blit(playerimg,P1.rect)
+		screen.blit(playerimg,P1.rect)
 
 	pygame.display.update()
 pygame.quit()
