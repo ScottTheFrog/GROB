49931e64bd7dc61e3c08eed7cc645f98cbf23e22
ScottTheFrog <55336944+ScottTheFrog@users.noreply.github.com>
2020-04-07 02:11:43 -0400

    Update Class.py

--- Class.py
+++ Class.py
@@ -1,19 +1,37 @@
+import Menu
 import pygame
 import random
+import math
 screen = pygame.display.set_mode([640,480])
 allrects = {}
+m = Menu.loadmenu("NOCOOKIESFORYOU.mp3")
 class Grob():
 	def __init__(self,x,y,health,speed,state):
+		self.playerimg = pygame.image.load("grob.png")
+		self.playerimg = pygame.transform.scale(self.playerimg, (32, 30))
+		self.playerimg = pygame.transform.rotate(self.playerimg, 180)
 		self.x 			= x
 		self.y			= y
-		self. health 	= health
+		self.health 	= health
+		self.healthfont	= pygame.font.Font("freesansbold.ttf",50)
 		self.speed	 	= speed
 		self.state 		= state
 		self.state 		= 2
 		self.running	= True
+		self.rotation 	= 0
+		self.god		= True
+		self.count		= 30
 	def move(self):
-		self.speed *= 1.0005
+		self.healthtxt = self.healthfont.render(str(self.health),0,[0,255,255])
+		screen.blit(self.healthtxt,[64,64])
+		if self.speed <= 25:
+			self.speed*= 1.0002
+
 		self.rect = pygame.Rect(self.x,self.y,12,10)
+		if self.rotation == 0:
+			screen.blit(self.playerimg,self.rect)
+		else:
+			screen.blit(self.playerimg,[self.rect[0],self.rect[1]-14,self.rect[2],self.rect[3]])
 
 		if self.running :	
 			for i in allrects:
@@ -28,20 +46,35 @@ class Grob():
 			self.state = 2
 
 		if self.state == 1:
+			self.rotation = 0
 			if self.y != 32:
 				self.y -= self.speed*4
 			if self.y < 32:
+				self.playerimg = pygame.transform.rotate(self.playerimg, 180)
 				self.y = 32
+
 		if self.state == 2:
+			self.rotation = 180
 			if self.y != 448:
 				self.y += self.speed*4
 			if self.y > 448:
+				self.playerimg = pygame.transform.rotate(self.playerimg, 180)
 				self.y = 448
+
 
 	def col(self):
 		for i in allrects:
 			if self.rect.colliderect(allrects.get(i)):
-				pygame.draw.rect(screen,[255,0,0],[0,0,640,480])
+				pygame.draw.rect(screen,[255,0,255],[0,0,640,480])
+				if self.god:
+					if self.count >= 0:
+						self.count -=1
+					else:
+						self.count = 30
+						self.god = False
+				else:
+					self.health -= 1
+
 class Block():
 	def __init__(self,x,y,w,h,rectname):
 		self.x 			= x
@@ -56,7 +89,8 @@ class Block():
 			allrects[self.rectname] = self.rect
 			self.rect = allrects.get(self.rectname)
 
-		pygame.draw.rect(screen,[120,0,0],self.rect)
+		pygame.draw.rect(screen,[255,0,255],self.rect)
+		
 
 	def random(self):
 		if self.rect[0] < -40:
