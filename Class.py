5800fa081d15e8b9080a2d7bc32a44c7e3a8ad9e
ScottTheFrog <55336944+ScottTheFrog@users.noreply.github.com>
2020-04-11 01:28:38 -0400

    Update Class.py

--- Class.py
+++ Class.py
@@ -13,7 +13,7 @@ class Grob():
 		self.x 			= x
 		self.y			= y
 		self.health 	= health
-		self.healthfont	= pygame.font.Font("freesansbold.ttf",50)
+		self.healthfont	= pygame.font.Font("freesansbold.ttf",30)
 		self.speed	 	= speed
 		self.state 		= state
 		self.state 		= 2
@@ -21,11 +21,16 @@ class Grob():
 		self.rotation 	= 0
 		self.god		= True
 		self.count		= 30
+		self.meters		= 0
 	def move(self):
 		self.healthtxt = self.healthfont.render(str(self.health),0,[0,255,255])
-		screen.blit(self.healthtxt,[64,64])
+		self.pointstxt = self.healthfont.render(str(round(self.meters/100)),0,[255,0,255])
+		self.meters += self.speed
+		screen.blit(self.healthtxt,[4,60])
+		screen.blit(self.pointstxt,[4,32])
 		if self.speed <= 25:
-			self.speed*= 1.0002
+			self.speed*= 1.0005
+		self.meters += self.speed
 
 		self.rect = pygame.Rect(self.x,self.y,12,10)
 		if self.rotation == 0:
@@ -34,6 +39,7 @@ class Grob():
 			screen.blit(self.playerimg,[self.rect[0],self.rect[1]-14,self.rect[2],self.rect[3]])
 
 		if self.running :	
+			
 			for i in allrects:
 				i2 = allrects.get(i)
 				i2[0] -= self.speed
@@ -66,14 +72,16 @@ class Grob():
 		for i in allrects:
 			if self.rect.colliderect(allrects.get(i)):
 				pygame.draw.rect(screen,[255,0,255],[0,0,640,480])
-				if self.god:
-					if self.count >= 0:
-						self.count -=1
-					else:
-						self.count = 30
-						self.god = False
-				else:
+				if not self.god:
 					self.health -= 1
+					self.god = True
+		if self.god:
+				if self.count >= 0:
+					self.count -=1
+				else:
+					self.count = 30
+					self.god = False
+
 
 class Block():
 	def __init__(self,x,y,w,h,rectname):
